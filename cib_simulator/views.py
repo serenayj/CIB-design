b3e5725412521989699b65eb2db888bc3d37afaf
serenayj <yug125@psu.edu>
2015-05-18 14:02:42 -0400

    May 18

--- cib_simulator/views.py
+++ cib_simulator/views.py
@@ -12,19 +12,49 @@ def index123(request):
 #def registration(request):
 
 
-def loginajax(request):
+def loginajax(request):                                                                                                                        
+    if request.method == 'POST':                                                                                                            
+        request.session.set_test_cookie()                                                                                                   
+        login_form = AuthenticationForm(request, request.POST)                                                                              
+        if login_form.is_valid():                                                                                                           
+            if request.is_ajax:                                                                                                             
+                user = django_login(request, login_form.get_user())                                                                         
+                if user is not None:                                                                                                        
+                    return HttpResponse(request.REQUEST.get('next', '/'))   
+        return HttpResponseForbidden() # catch invalid ajax and all non ajax  
+        # if not valid users, return registration form                                                      
+    else:                                                                                                                                   
+        return login_form;
+        #login_form = AuthenticationForm()                                                                                                                                        
+    #c = Context({                                                                                                                           
+        """'next': request.REQUEST.get('next'),                                                                                                
+        'login_form': login_form,                                                                                                                         
+        'request':request,                                                                                                                  
+    })                                                                                                                                      
+    return render_to_response('login.html', c, context_instance=RequestContext(request))"""
+
+def registraion(request):
     if request.method == "POST":
-        form = AdvertForm(request.POST)
+        content = login_form = AuthenticationForm(request, request.POST)
+        newRecord = ({
+            Userid= request.POST.get("Userid")
+            username= request.POST.get("username")
+            Password = request.POST.get("Password")
+            E-mail = request.POST.get("Email")
+        })
+        class User(models.Model):
+            newRecord.save()
+            return render_to_response('index.html', context_instance=RequestContext(request))
 
-        message = 'something wrong!'
-        if(form.is_valid()):
-            print(request.POST['title'])
-            message = request.POST['title']
 
-        return HttpResponse(json.dumps({'message': message}))
+def shareboards(request):
 
-    return render_to_response('contact/advert.html',
-            {'form':AdvertForm()}, RequestContext(request))
+
+def chathistory(request):
+
+
+def queries(request):
+
 
    
 '''
