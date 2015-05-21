38ef009491f2b1f5344b41975475db3e06b3e5d7
serenayj <yug125@psu.edu>
2015-05-21 19:17:09 -0400

    May 21

--- cib_simulator/urls.py
+++ cib_simulator/urls.py
@@ -2,16 +2,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls import patterns
 
-from views import *
+from sampleapp.views import *
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^$', views.login),
+	url(r'^$', 'cib_simulator.views.login',name = "login"),
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^index',views.index123),
+    url(r'^index','cib_simulator.views.index123',name='index'),
 
-    url(r'^registration',views.registration),
+    #url(r'^registration',views.registration),
 ]
--- cib_simulator/views.py
+++ cib_simulator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.template import Context
 from django.db.models import Q
 from django.contrib.auth import models
-from django.contrib.auth import AuthenticationForm
+"""from django.contrib.auth import AuthenticationForm"""
 
 #this is a form which might turn to index
 def index123(request):    
@@ -12,11 +12,12 @@ def index123(request):
 
 #def registration(request):
 
-
 def login(request):
-    """if request.user.is_authenticated():
+    return render(request,'login.html')
+    
+    if request.user.is_authenticated():
         request.session['userid'] = request.user.id
-        return HttpResponseRedirect('/index/')"""
+        return HttpResponseRedirect('/index/')
                                                                                                                                 
     if request.method == 'POST':                                                                                                            
         request.session.set_test_cookie()                                                                                                   
@@ -33,18 +34,18 @@ def login(request):
         else:                                                                                                                                   
             return login_form;
         #login_form = AuthenticationForm()                                                                                                                                        
-    #c = Context({                                                                                                                           
-        """'next': request.REQUEST.get('next'),                                                                                                
+    """#c = Context({                                                                                                                           
+        next': request.REQUEST.get('next'),                                                                                                
         'login_form': login_form,                                                                                                                         
         'request':request,                                                                                                                  
         })                                                                                                                                      
-return render_to_response('login.html', c, context_instance=RequestContext(request))"""
+return render_to_response('login.html', c, context_instance=RequestContext(request))
 
 def registraion(request):
     if request.method == "POST":
         login_form = AuthenticationForm(request, request.POST)
         newRecord = request.get("userid","username","password","email")
-        from models import User:  #warning: invalid syntax 
+        from sampleapp.models import class User:  #warning: invalid syntax 
             newRecord.save()
             return render_to_response('index.html', context_instance=RequestContext(request))
 
@@ -58,8 +59,6 @@ def registraion(request):
 #def queries(request):
 
 
-   
-'''
     if request.user.is_authenticated():
         request.session['currUserId'] = request.user.id
         message_ids   = request.REQUEST.getlist('messages')
@@ -73,4 +72,6 @@ def registraion(request):
         request.session['currForumId'] = -1
         return render(request, 'dashboard/index.html', Context({'USERNAME': ''}))
     return redirect('/login')   # this may never be triggered.
-'''
+
+
+"""
